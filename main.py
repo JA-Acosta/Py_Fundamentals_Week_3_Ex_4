e0d92c4f8f5cd96cc949b4d6503076f55b141ede
JAAR <joe.aaron.acosta@outlook.com>
2023-08-11 23:30:50 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+>>> JAAR
+>>> 8/9/2023
+>>> Practicing Fundamentals Program 16
+>>> Version 0
+'''
+
+'''
+>>> Generates a program that takes a csv file containing a list of users and their emails and creates a copy of the csv file that is sorted based on last name.
+'''
+
+def main() :
+    pass
+
+if __name__ == "__main__" :
+    main()
